780a2ce7a0702d796c4161c59b2f46ca972066ad
Kay Cee <8.kaycee@gmail.com>
2023-11-27 21:12:31 +0100

    feat: reload data from database

--- back/app/storage/db/__init__.py
+++ back/app/storage/db/__init__.py
@@ -7,7 +7,7 @@ class DBStorage:
     __engine = None
     __session = None
 
-    def __init__():
+    def __init__(self):
         ''' Initialize the Database storage '''
         self.__engine = create_engine(settings.SQLALCHEMY_DATABASE_URI,
             pool_pre_ping=True)
@@ -17,4 +17,9 @@ class DBStorage:
             self.__engine = create_engine(
                 f'{settings.SQLALCHEMY_DATABASE_URI}_test', pool_pre_ping=True)
 
-
+    def reload():
+        ''' Reload data from the database '''
+        Base.metadata.create_all(self.__engine)
+        sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sess_factory)
+        self.__session = Session
